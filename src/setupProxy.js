c788e83acd8da985ad5d9b7d280120d784ba5e1a
VicThor-wpp <victor.aragon@vmlyr.com>
2024-04-09 15:31:17 -0300

    Update de url de conexion a: fetch('http://127.0.0.1:5000/upload_data

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
     createProxyMiddleware('/api', {
-      target: 'http://localhost:5000',
+      target: 'http://0.0.0.0:5000',
       changeOrigin: true,
     })
   );
